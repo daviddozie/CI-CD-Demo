8ddd71e07315c9c875bb541a4007eb6c3a51bdae
daviddozie <davidmgbede75@gmail.com>
2024-02-29 02:16:49 -0800

    adding a spec.js file to pipeline

---
+++ src/tests/components/App.spec.js
@@ -0,0 +1,7 @@
+import { describe, it, expect } from "vitest";
+
+describe('simple tesxt coverage', () => {
+    it('it should display a text on my page', () => {
+        expect(true).toBeTruthy();
+    })
+})
